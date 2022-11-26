0e8f7774213ddcf619af7ee654d850544ca2900c
KG <KG@DESKTOP-H2S6LPC>
2022-11-26 15:30:48 +0200

    Добавьте файлы проекта.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,134 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "W:\Университет\ИПЗ\laba3\laba3\laba3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1s)
+	{
+	public:
+		// Check program number 1. The test will be successful if the interval is -10 to 10, n=4 and step 10 will be the expected values equal to y-values.
+		TEST_METHOD(solvingEquationSystems_getMinus10and10and4and10returned)
+		{
+			double x1 = -10;
+			double x2 = 10;
+			double n = 4;
+			double h = 10;
+
+			double y;
+
+			double expected[] = { 1, 5, 145 };
+
+			for (int x = x1, i = 0; x <= x2; x += h, i++)
+			{
+				funcY(x, n, y);
+				double actual = y;
+
+				Assert::AreEqual(expected[i], actual);
+			}
+		}
+
+	public:
+		// Check program number 2. The test will be successful if the interval is 17 to 100, n=5 and step 15 will be the expected values equal to y-values.
+		TEST_METHOD(solvingEquationSystems_get17and100and5and15returned)
+		{
+			double x1 = 17;
+			double x2 = 100;
+			double n = 5;
+			double h = 15;
+
+			double y;
+
+			double expected[] = { 677, 2702, 6077, 10802, 16877, 24302 };
+
+			for (int x = x1, i = 0; x <= x2; x += h, i++) {
+				funcY(x, n, y);
+				double actual = y;
+
+				Assert::AreEqual(expected[i], actual);
+			}
+		}
+
+	public:
+		//The test will check the input values. The test is successful if all entered values pass the check.
+		TEST_METHOD(сheckChecks_get120and1221and4and10_exceptionNotThrown)
+		{
+			double x1 = 120;
+			double x2 = 1221;
+			double n = 4;
+			double h = 10;
+
+			try
+			{
+				сheck(x1, x2, n, h);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+
+	public:
+		//Checks for error output and program ending when wrong values are entered. The test will succeed if the check fails. 
+		TEST_METHOD(сheckChecks_get10and11and4an10_exceptionThrown)
+		{
+			double x1 = 10;
+			double x2 = 11;
+			double n = 4;
+			double h = 10;
+
+			try
+			{
+				сheck(x1, x2, n, h);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+
+	public:
+		//Checks for error output and program ending when wrong values are entered. The test will succeed if the check fails. 
+		TEST_METHOD(сheckChecks_get10and15and4an10_exceptionThrown)
+		{
+			double x1 = 10;
+			double x2 = 15;
+			double n = 4;
+			double h = 10;
+
+			try
+			{
+				сheck(x1, x2, n, h);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	public:
+		//An example of failure to pass a test.
+		TEST_METHOD(solvingEquationSystems2_get17and100and5and15returned)
+		{
+			double x1 = 17;
+			double x2 = 100;
+			double n = 5;
+			double h = 15;
+
+			double y;
+
+			double expected[] = { 1000000, 1000000, 1000000, 1000000, 1000000, 1000000 }; //The values are too big and the same everywhere. This is impossible.
+
+			for (int x = x1, i = 0; x <= x2; x += h, i++) {
+				funcY(x, n, y);
+				double actual = y;
+
+				Assert::AreEqual(expected[i], actual);
+			}
+		}
+	};
+}
---
+++ laba3/laba3.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+// ADDED: function outputToFile / ability to write to file
+void outputToFileFinction(int x, int y) {
+    ofstream fout;
+    fout.open("outputToFile.txt", ios_base::app);
+    fout << "X= " << x << " Y= " << y << endl;
+    fout.close();
+};
+
+// ADDED: function read
+void read(double& x1, double& x2, double& n, double& h) //Function for entering values by the user.
+{
+    cout << "Enter the beginning of interval x1 and the end of interval x2: ";
+    cin >> x1 >> x2;
+    cout << "Enter the value of n and the step h: ";
+    cin >> n >> h;
+}
+
+// ADDED: function check
+int сheck(double& x1, double& x2, double& n, double& h) //Function for checking the input values.
+{
+    if (x2 < x1)
+    {
+        throw "ERROR: x2 is less than x1.";
+    }
+    else if (h > (x2 - x1))
+    {
+        throw "ERROR: h is less than x2-x1.";
+    }
+    else if (n != int(n))
+    {
+        throw "ERROR: n must me integer.";
+    }
+}
+
+// ADDED: function xMoreOrEqualToZero
+double xMoreOrEqualToZero(double x, double n, double y) //Function for the case when x is less than 0.
+{
+    for (int i = 1; i <= (n - 2); i++)
+    {
+        y += pow((i - x), 2);
+    }
+    return y;
+}
+
+// ADDED: function xLessThanZero
+double xLessThanZero(double n, double y) //Function for the case when x is greater than or equal to 0.
+{
+    for (int i = 0; i <= n; i++)
+    {
+        int p = 1;
+        for (int j = i; j <= (n - 1); j++)
+        {
+            p *= (i - j);
+        }
+        y += p;
+    }
+    return y;
+}
+
+// ADDED: function funcY
+int funcY(double x, double n, double& y)
+{
+    n = int(n);
+    y = 0;
+
+    if (x >= 0)
+    {
+        y = xMoreOrEqualToZero(x, n, y);
+        return 0;
+    }
+    else if (x < 0)
+    {
+        y = xLessThanZero(n, y);
+        return 0;
+    }
+}
+
+
+int main()
+{
+    double x1, x2;
+    double n, h;
+    read(x1, x2, n, h);
+
+    char outputToFile;
+    cout << "Do you want to output the result to a separate outputToFile.txt file? Enter y(i.e. yes)  or n(i.e. not): " << endl;
+    cin >> outputToFile;
+
+    try {
+        сheck(x1, x2, n, h);
+
+        double y;
+        for (int x = x1; x <= x2; x += h) {
+            funcY(x, n, y);
+            cout << " X= " << x << " Y= " << y << endl;
+            if (outputToFile == 'y')
+            {
+                outputToFileFinction(x, y);
+            }
+        }
+    }
+    catch (const char* ex)
+    {
+        cout << ex << endl;
+        return -2;
+    }
+    return 0;
+}
